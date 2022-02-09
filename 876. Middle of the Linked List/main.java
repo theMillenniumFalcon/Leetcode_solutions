edc326f90266dc93f6e787e9389301e3e9e968a5
Nishank <nishankpr2002@gmail.com>
2022-02-09 22:32:28 +0530

    updated

---
+++ 876. Middle of the Linked List/main.java
@@ -0,0 +1,34 @@
+public class main {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
