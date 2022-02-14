f49d236290bf58cf3d8e52adb4cfba8edb22c55e
Nishank <nishankpr2002@gmail.com>
2022-02-14 19:39:03 +0530

    updated

---
+++ 25. Reverse Nodes in k-Group/main.java
@@ -0,0 +1,61 @@
+public class main {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k <= 1) {
+            return head;
+        }
+
+        ListNode current = head;
+        ListNode prev = null;
+
+        while (true) {
+            ListNode last = prev;
+            ListNode newEnd = current;
+
+            ListNode next = current.next;
+            for (int i = 0; current != null && i < k; i++) {
+                current.next = prev;
+                prev = current;
+                current = next;
+                if (next != null) {
+                    next = next.next;
+                }
+            }
+
+            if (last != null) {
+                last.next = prev;
+            } else {
+                head = prev;
+            }
+
+            newEnd.next = current;
+            if (current == null) {
+                break;
+            }
+
+            
+
+        }
+        return head;
+    }
+}
