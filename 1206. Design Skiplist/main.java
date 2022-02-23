63292ee4196008fab8114c4e5bfd3ee7763a0ac5
Nishank <nishankpr2002@gmail.com>
2022-02-23 21:55:33 +0530

    updated

---
+++ 1206. Design Skiplist/main.java
@@ -0,0 +1,18 @@
+class Skiplist {
+
+    public Skiplist() {
+        
+    }
+    
+    public boolean search(int target) {
+        
+    }
+    
+    public void add(int num) {
+        
+    }
+    
+    public boolean erase(int num) {
+        
+    }
+}
