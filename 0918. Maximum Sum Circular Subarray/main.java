5d93ec2da0e7b130e5947be327bc6767e7e84e44
Nishank <nishankpr2002@gmail.com>
2022-03-11 21:31:45 +0530

    updated

--- 0918. Maximum Sum Circular Subarray/main.java
+++ 0918. Maximum Sum Circular Subarray/main.java
@@ -1,9 +1,25 @@
 public class main {
     public static void main(String[] args) {
-        
+
     }
 
     public int maxSubarraySumCircular(int[] nums) {
-        
+        int max = nums[0];
+        int min = nums[0];
+        int cMax = nums[0];
+        int cMin = nums[0];
+        int sum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            cMax = nums[i] + Math.max(cMax, 0);
+            max = Math.max(max, cMax);
+
+            cMin = nums[i] + Math.min(cMin, 0);
+            min = Math.min(min, cMin);
+
+            sum += nums[i];
+        }
+
+        return sum == min ? max : Math.max(max, sum - min);
     }
 }
