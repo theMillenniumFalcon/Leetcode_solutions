c96505c53b1a82a5535d6db86b66f636a96f603d
Nishank <nishankpr2002@gmail.com>
2022-03-12 19:30:31 +0530

    updated

--- Solutions/0241. Different Ways to Add Parentheses/main.java
+++ Solutions/0241. Different Ways to Add Parentheses/main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class main {
@@ -6,6 +7,31 @@ public class main {
     }
 
     public List<Integer> diffWaysToCompute(String expression) {
+        List<Integer> list = new ArrayList<>();
         
+        for(int i = 0; i < expression.length(); i++)
+        {
+            char ch = expression.charAt(i);
+            if(ch == '-' || ch == '+' || ch == '*')
+            {
+                List<Integer> left = diffWaysToCompute(expression.substring(0, i));
+                List<Integer> right = diffWaysToCompute(expression.substring(i+1));
+                for(int m : left)
+                {
+                    for(int n : right)
+                    {
+                        if(ch == '-')
+                            list.add(m-n);
+                        else if(ch == '+')
+                            list.add(m+n);
+                        else list.add(m*n);
+                    }
+                }
+            }
+        }
+        if(list.size() == 0)
+            list.add(Integer.valueOf(expression));
+            
+        return list;
     }
 }
