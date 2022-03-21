2c5c4893ad967cd6e11d40c91ecc8cb86af9b46a
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-03-21 11:17:06 -0700

    updated

--- Solutions/0543. Diameter of Binary Tree/main.java
+++ Solutions/0543. Diameter of Binary Tree/main.java
@@ -23,6 +23,18 @@ public class main {
     }
 
     public int diameterOfBinaryTree(TreeNode root) {
-        
+        int[] Max = new int[1];
+        diameter(root, Max);
+        return Max[0];
+    }
+
+    public int diameter(TreeNode root, int[] Max) {
+        if (root == null) return 0;
+
+        int leftHeight = diameter(root.left, Max);
+        int rightHeight = diameter(root.right, Max);
+        Max[0] = Math.max(Max[0], leftHeight + rightHeight);
+    
+        return 1 + Math.max(leftHeight, rightHeight);
     }
 }
