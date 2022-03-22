40994194145188ccc764ec6b77842024bea917c7
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-03-21 20:52:33 -0700

    updated

---
+++ Solutions/0700. Search in a Binary Search Tree/main.java
@@ -0,0 +1,35 @@
+public class main {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) return root;
+
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        }
+
+        return searchBST(root.right, val);
+    }
+}
