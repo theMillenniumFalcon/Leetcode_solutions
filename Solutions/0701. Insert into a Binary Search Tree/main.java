7b0c06dc761965c4f80f2d749a3552f08dca9205
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-03-21 21:05:51 -0700

    updated

---
+++ Solutions/0701. Insert into a Binary Search Tree/main.java
@@ -0,0 +1,37 @@
+public class main {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        } else if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+
+        return root;
+    }
+} 
