e0c1984c0d80bfe338d538fdfd838b6494a79853
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-03-23 21:38:40 +0530

    updated

---
+++ Solutions/0295. Find Median from Data Stream/main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        
+    }
+    
+    public class MedianFinder {
+        PriorityQueue<Integer> maxHeap;
+        PriorityQueue<Integer> minHeap;
+    
+        public MedianFinder() {
+            maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+            minHeap = new PriorityQueue<>();
+        }
+    
+        public void addNum(int num) {
+            minHeap.add(num);
+            maxHeap.add(minHeap.poll());
+    
+            if (maxHeap.size() > minHeap.size()) {
+                minHeap.add(maxHeap.poll());
+            }
+        }
+    
+        public double findMedian() {
+            if (minHeap.size() > maxHeap.size()) {
+                return (double) minHeap.peek();
+            } else {
+                return (double) minHeap.peek() / 2d + (double) maxHeap.peek() / 2d;
+            }
+        }
+    }
+}
+
