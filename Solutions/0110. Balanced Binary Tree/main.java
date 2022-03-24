06bcf0d7dc1128b15788e5a43e6e5cb9e2a5dc21
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-03-25 01:19:14 +0530

    6th project completed

---
+++ Solutions/0110. Balanced Binary Tree/main.java
@@ -0,0 +1,69 @@
+public class main {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    // * BRUTEFORCE
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        if (Math.abs(rightHeight - leftHeight) > 1) return false;
+
+        Boolean left = isBalanced(root.left);
+        Boolean right = isBalanced(root.right);
+
+        if (!left || !right) return false;
+
+        return true;
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
+
+    // * OPTIMIZED
+    public boolean isBalanced2(TreeNode root) {
+        return dfsHeight(root) != -1;
+    }
+
+    public int dfsHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = dfsHeight(root.left);
+        int rightHeight = dfsHeight(root.right);
+        if (leftHeight == -1) return -1;
+        if (rightHeight == -1) return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+        
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+} 
