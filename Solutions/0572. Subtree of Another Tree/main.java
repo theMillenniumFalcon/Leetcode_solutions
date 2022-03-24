473ee9bff82b024b4bc60024870c5dd13462651a
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-03-24 15:40:27 +0530

    updated

---
+++ Solutions/0572. Subtree of Another Tree/main.java
@@ -0,0 +1,47 @@
+import javax.lang.model.element.Element;
+import javax.xml.crypto.Data;
+
+public class main {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        } else if (isSameTree(root, subRoot)) {
+            return true;
+        } else {
+            return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        }
+    }
+
+    public boolean isSameTree(TreeNode root, TreeNode subRoot) {
+        if (root == null || subRoot == null) {
+            return root == null && subRoot == null;
+        } else if (root.val == subRoot.val) {
+            return isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right);
+        } else {
+            return false;
+        }
+    }
+} 
