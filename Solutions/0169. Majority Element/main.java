b88b663714069aec4561dd2a5c4718dfade7f6ed
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-03-26 03:56:14 +0530

    updated

---
+++ Solutions/0169. Majority Element/main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > nums.length / 2) {
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
+}
