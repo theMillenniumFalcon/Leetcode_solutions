9db4128a85a450b55e0ae9b8e838680268fc3ac9
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-03-28 17:03:29 +0530

    updated

---
+++ Solutions/1863. Sum of All Subset XOR Totals/main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+        
+    }
+
+    public int subsetXORSum(int[] nums) {
+        return helper(nums, 0, 0);
+    }
+
+    public int helper(int[] nums, int level, int currentXOR) {
+        if (level == nums.length) return currentXOR;
+
+        int include = helper(nums, level + 1, currentXOR ^ nums[level]);
+        int exclude = helper(nums, level + 1, currentXOR);
+
+        return include + exclude;
+    }
+}
