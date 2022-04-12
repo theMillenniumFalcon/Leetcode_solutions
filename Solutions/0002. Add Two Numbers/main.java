c6a9ed72a03eb2e5027298f8a8329a797a56bf98
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-04-12 11:42:47 +0530

    updated

--- Solutions/0002. Add Two Numbers/main.java
+++ Solutions/0002. Add Two Numbers/main.java
@@ -22,36 +22,29 @@ public class main {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy_head = new ListNode(0);
-        ListNode l3 = dummy_head;
+        ListNode dummy = new ListNode();
+        ListNode temp = dummy;
         int carry = 0;
 
-        while (l1 != null || l2 != null) {
-            int l1_val = (l1 != null) ? l1.val : 0;
-            int l2_val = (l2 != null) ? l2.val : 0;
-
-            int current_sum = l1_val + l2_val + carry;
-            carry = current_sum / 10;
-            int last_digit = current_sum % 10;
-
-            ListNode new_node = new ListNode(last_digit);
-            l3.next = new_node;
-
+        while (l1 != null || l2 != null || carry == 1) {
+            int sum = 0;
             if (l1 != null) {
+                sum += l1.val;
                 l1 = l1.next;
             }
+
             if (l2 != null) {
+                sum += l2.val;
                 l2 = l2.next;
             }
-            l3 = l3.next;
+
+            sum += carry;
+            carry = sum / 10;
+            ListNode node = new ListNode(sum % 10);
+            temp.next = node;
+            temp = temp.next;
         }
 
-        if (carry > 0) {
-            ListNode new_node = new ListNode(carry);
-            l3.next = new_node;
-            l3 = l3.next;
-        }
-
-        return dummy_head.next;
+        return dummy.next;
     }
 }
