1dbd605ddea4f3d06302a8e4f07edf5bd9f64156
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-04-13 19:51:13 +0530

    updated

--- Solutions/0142. Linked List Cycle II/main.java
+++ Solutions/0142. Linked List Cycle II/main.java
@@ -15,60 +15,22 @@ public class main {
     }
 
     public ListNode detectCycle(ListNode head) {
-        int length = 0;
         ListNode fast = head;
         ListNode slow = head;
 
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
-            if (fast == slow) {
-                length = lengthCycle(slow);
-                break;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
             }
         }
 
-        if (length == 0) {
-            return null;
-        }
-        
-        // find the start node
-        ListNode f = head;
-        ListNode s = head;
-
-        while (length > 0) {
-            s = s.next;
-            length--;
-        }
-
-        // keep moving forward and they will meet at cycle start
-        while (f != s) {
-            f = f.next;
-            s = s.next;
-        }
-        return s;
-    }
-
-    // finding the length of the cycle
-    public int lengthCycle(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-
-            if (fast == slow) {
-                // calculate the length
-                ListNode temp = slow;
-                int length = 0;
-                do {
-                    temp = temp.next;
-                    length ++;
-                } while (temp != slow);
-                return length;
-            }
-        }
-        return 0;
+        return null;
     }
 }
