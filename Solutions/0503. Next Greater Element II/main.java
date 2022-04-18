bac42cd595ebe9ef13c8f48e1723cc01fe9db78f
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-04-18 07:53:17 +0530

    updated

--- Solutions/0503. Next Greater Element II/main.java
+++ Solutions/0503. Next Greater Element II/main.java
@@ -3,24 +3,24 @@ import java.util.Stack;
 
 public class main {
     public static void main(String[] args) {
-        
+
     }
 
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        int[] output_arr = new int[n];
-        Arrays.fill(output_arr, -1);
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
 
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < (2*n - 1); i++) {
+        for (int i = 0; i < (2 * n - 1); i++) {
             while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n]) {
-                output_arr[stack.pop()] = nums[i % n]; 
+                res[stack.pop()] = nums[i % n];
             }
             if (i < n) {
                 stack.push(i);
             }
         }
 
-        return output_arr;
+        return res;
     }
 }
