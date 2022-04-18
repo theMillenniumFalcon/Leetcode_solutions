5916d11f20cec4583563c961d4e031cdf9a8408f
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-04-18 23:59:48 +0530

    updated

---
+++ Solutions/0876. Middle of the Linked List/main.go
@@ -0,0 +1,17 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+	}
+
+	return slow
+}
