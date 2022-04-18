efe10db71b3c8236661466e61f97b93b33662a6d
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-04-18 06:49:08 +0530

    updated

---
+++ Solutions/0232. Implement Queue using Stacks/main.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class MyQueue {
+
+    Stack<Integer> input;
+    Stack<Integer> output;
+
+    public MyQueue() {
+        input = new Stack<>();
+        output = new Stack<>();
+    }
+
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public int pop() {
+        if (!output.isEmpty()) {
+            return output.pop();
+        } else {
+            while (!input.isEmpty()) {
+                output.push(input.peek());
+                input.pop();
+            }
+            return output.pop();
+        }
+    }
+
+    public int peek() {
+        if (!output.isEmpty()) {
+            return output.peek();
+        } else {
+            while (!input.isEmpty()) {
+                output.push(input.peek());
+                input.pop();
+            }
+            return output.peek();
+        }
+    }
+
+    public boolean empty() {
+        return input.isEmpty() && output.isEmpty();
+    }
+}
