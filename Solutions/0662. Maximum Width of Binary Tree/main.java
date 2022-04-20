dcafd1280ea16f11d88bdf8dd2e79d6f4c142f2d
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-04-20 19:49:01 +0530

    updated

---
+++ Solutions/0662. Maximum Width of Binary Tree/main.java
@@ -0,0 +1,28 @@
+public class main {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public int widthOfBinaryTree(TreeNode root) {
+
+    }
+}
---
+++ Solutions/0706. Design HashMap/main.java
@@ -0,0 +1,18 @@
+class MyHashMap {
+
+    public MyHashMap() {
+        
+    }
+    
+    public void put(int key, int value) {
+        
+    }
+    
+    public int get(int key) {
+        
+    }
+    
+    public void remove(int key) {
+        
+    }
+}
