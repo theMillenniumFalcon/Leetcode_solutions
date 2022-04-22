f7033f9748d2fb637595cff07bbed429b62e90f8
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-04-23 01:04:49 +0530

    updated

---
+++ Solutions/0151. Reverse Words in a String/main.java
@@ -0,0 +1,24 @@
+public class main {
+    public static void main(String[] args) {
+        
+    }
+
+    public String reverseWords(String s) {
+        String result = new String();
+        int i = 0;
+        int n = s.length();
+
+        while (i < n) {
+            while (i < n && s.charAt(i) == ' ') i++;
+            if (i >= n) break;
+            int j = i + 1;
+            while (j < n && s.charAt(j) != ' ') j++;
+            String sub = s.substring(i, j);
+            if (result.length() == 0) result = sub;
+            else result = sub + " " + result;
+            i = j + 1;
+        }
+
+        return result;
+    }
+}
