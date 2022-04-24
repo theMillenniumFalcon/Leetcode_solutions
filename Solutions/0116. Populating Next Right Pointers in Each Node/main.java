98dcae4572ccd378e91b1ea03a1f1a8abdbdb756
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-04-24 23:14:57 +0530

    updated

--- Solutions/0116. Populating Next Right Pointers in Each Node/main.java
+++ Solutions/0116. Populating Next Right Pointers in Each Node/main.java
@@ -25,6 +25,23 @@ public class main {
     }
 
     public Node connect(Node root) {
-        
+        if (root == null) return null;
+        Node curr = root;
+        Node next = root.left;
+
+        while (curr != null && next != null) {
+            curr.left.next = curr.right;
+            if (curr.next != null) {
+                curr.right.next = curr.next.left;
+            }
+
+            curr = curr.next;
+            if (curr == null) {
+                curr = next;
+                next = curr.left;
+            }
+        }
+
+        return root;
     }
 }
