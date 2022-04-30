09fe1f721134cec691e0c8f1ef8353f780183525
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-04-30 05:53:38 +0530

    updated

---
+++ Solutions/1721. Swapping Nodes in a Linked List/main.java
@@ -0,0 +1,45 @@
+public class main {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode swapNodes(ListNode head, int k) {
+        k = k - 1;
+        ListNode firstN = head;
+        while (k > 0) {
+            firstN = firstN.next;
+            k--;
+        }
+
+        ListNode f = firstN;
+        ListNode secondN = head;
+        while (f.next != null) {
+            secondN = secondN.next;
+            f = f.next;
+        }
+
+        int temp = firstN.val;
+        firstN.val = secondN.val;
+        secondN.val = temp;
+
+        return head;
+    }
+
+}
