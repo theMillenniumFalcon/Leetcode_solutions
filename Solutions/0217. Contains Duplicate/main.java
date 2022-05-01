ea096b3a87af458285a13bddca49b229296fa689
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-05-02 02:38:16 +0530

    updated

--- Solutions/0217. Contains Duplicate/main.java
+++ Solutions/0217. Contains Duplicate/main.java
@@ -1,33 +1,22 @@
+import java.util.*;
+
 public class main {
     public static void main(String[] args) {
-        
+
     }
 
-    static boolean containsDuplicate(int[] nums) {
-        insertion(nums);
-        for (int k = 0; k < nums.length - 1; k++) {
-            if (nums[k] == nums[k+1]) {
+    // TC : O(n)
+    // SC: O(n)
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
                 return true;
+            } else {
+                map.put(nums[i], i);
             }
         }
-        return false; 
-    }
 
-    static void insertion(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j > 0; j--) {
-                if (arr[j] < arr[j - 1]) {
-                    swap(arr, j, j - 1);
-                } else {
-                    break;
-                }
-            }
-        }
-    }
-
-    static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
+        return false;
     }
 }
