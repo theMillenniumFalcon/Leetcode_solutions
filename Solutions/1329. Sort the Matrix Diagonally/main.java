a52b6dd62fd290c4d6286240aaac8c25e91c336f
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-05-01 17:26:42 +0530

    updated

---
+++ Solutions/1329. Sort the Matrix Diagonally/main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+
+    // TC : O(m*n * log(D)) , D stand min(m, n)
+    // SC: O(m*n)
+    public int[][] diagonalSort(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int diagonalKey = i - j;
+                PriorityQueue<Integer> pq = map.getOrDefault(diagonalKey, new PriorityQueue<>());
+                pq.offer(mat[i][j]);
+                map.put(diagonalKey, pq);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int diagonalKey = i - j;
+                PriorityQueue<Integer> pq = map.get(diagonalKey);
+
+                mat[i][j] = pq.poll();
+            }
+        }
+
+        return mat;
+    }
+}
