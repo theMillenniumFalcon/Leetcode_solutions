0c7f205a5673e870ea3540242e27a6bc2b8b95d9
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-05-02 03:49:36 +0530

    updated

--- Solutions/0001. Two Sum/main.java
+++ Solutions/0001. Two Sum/main.java
@@ -1,22 +1,25 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class main {
     public static void main(String[] args) {
         
     }
 
+    // TC : O(n)
+    // SC: O(n)
     static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int compliment = target - nums[i];
             if (map.containsKey(compliment)) {
                 result[0] = i;
                 result[1] = map.get(compliment);
-                return result; 
+                return result;
             }
             map.put(nums[i], i);
         }
+
         return result;
     }
 }
