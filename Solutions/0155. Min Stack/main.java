443f5b1fe3550353d7be56647b71deaa96b5ff14
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-05-03 20:04:37 +0530

    updated

--- Solutions/0155. Min Stack/main.java
+++ Solutions/0155. Min Stack/main.java
@@ -1,28 +1,35 @@
-import java.util.Stack;
+import java.util.*;
 
+// TC : O(1)
+// SC: O(n)
 class MinStack {
 
-    Stack<Integer> stack = new Stack();
-    Stack<Integer> min_vals = new Stack();
-    
+    Stack<Integer> stack;
+    Stack<Integer> min_vals;
+
+    public MinStack() {
+        stack = new Stack<>();
+        min_vals = new Stack<>();
+    }
+
     public void push(int val) {
+        stack.push(val);
         if (min_vals.isEmpty() || val <= min_vals.peek()) {
             min_vals.push(val);
         }
-        stack.push(val);
     }
-    
+
     public void pop() {
         if (stack.peek().equals(min_vals.peek())) {
             min_vals.pop();
         }
         stack.pop();
     }
-    
+
     public int top() {
         return stack.peek();
     }
-    
+
     public int getMin() {
         return min_vals.peek();
     }
