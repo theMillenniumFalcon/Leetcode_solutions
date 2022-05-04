52ca52be56b692c1510b5646d576b355ee294b1e
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-05-04 19:38:39 +0530

    updated

---
+++ Solutions/0875. Koko Eating Bananas/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        
+    }
+
+    // TC : O(n)
+    // SC: O(1)
+    public int minEatingSpeed(int[] piles, int h) {
+        
+    }
+}
