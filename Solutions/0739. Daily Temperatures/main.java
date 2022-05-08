1bc87a484c0981072e20f2ed0fa8069ffec0ecaf
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-05-09 00:48:51 +0530

    updated

--- Solutions/0739. Daily Temperatures/main.java
+++ Solutions/0739. Daily Temperatures/main.java
@@ -5,15 +5,17 @@ public class main {
         
     }
 
+    // TC : O(n)
+    // SC: O(1)
     public int[] dailyTemperatures(int[] temperatures) {
         int len = temperatures.length;
         int[] ans = new int[len];
 
+        // we are storing indexes in the stack
         Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < len; i++) {
-            
-            while (!stack.empty() && temperatures[stack.peek()] <temperatures[i]) {
+            while (!stack.empty() && temperatures[stack.peek()] < temperatures[i]) {
                 // found an increasing value for stack.peek() index
                 ans[stack.peek()] = i - stack.peek();
                 stack.pop();
