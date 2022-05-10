81a6ca713b24ccabd6682ee52d393d00d1822d50
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-05-10 18:07:19 +0530

    updated

--- Solutions/0036. Valid Sudoku/main.java
+++ Solutions/0036. Valid Sudoku/main.java
@@ -5,18 +5,27 @@ public class main {
 
     }
 
+    // TC : O(n^2)
+    // SC: O(n^2)
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> set = new HashSet<>();
+        HashSet<String> set = new HashSet<String>();
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                char current_val = board[i][j];
-                if (current_val != '.') {
-                    if (!set.add(current_val + " found in row " + i) ||
-                    !set.add(current_val + " found in column " + j) ||
-                    !set.add(current_val + " found in sub-boxes " + i / 3 + "-" + j / 3)) {
+                if (board[i][j] != '.') {
+
+                    // Check whether HashSet contains duplicate elements in row and column
+                    if (set.contains("row" + i + board[i][j]) || set.contains("col" + j + board[i][j])) {
                         return false;
                     }
+                    set.add("row" + i + board[i][j]);
+                    set.add("col" + j + board[i][j]);
+
+                    // Check whether Box contains duplicate elements in it
+                    if (set.contains("box" + (i / 3) * 3 + j / 3 + board[i][j])) {
+                        return false;
+                    }
+                    set.add("box" + (i / 3) * 3 + j / 3 + board[i][j]);
                 }
             }
         }
