c9a42674cdbed5f1997836fa11a31fd8d5532414
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-05-11 08:36:48 +0530

    updated

---
+++ Solutions/0042. Trapping Rain Water/main.java
@@ -0,0 +1,31 @@
+public class main {
+    public static void main(String[] args) {
+        
+    }
+
+    // TC : O(n)
+    // SC: O(1)
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int left = 0, right = height.length - 1;
+        int leftMax = height[left], rightMax = height[right];
+        int res = 0;
+
+        while (left < right) {
+            if (leftMax < rightMax) {
+                left++;
+                leftMax = Math.max(leftMax, height[left]);
+                res += leftMax - height[left];
+            } else {
+                right--;
+                rightMax = Math.max(rightMax, height[right]);
+                res += rightMax - height[right];
+            }
+        }
+
+        return res;
+    }
+}
