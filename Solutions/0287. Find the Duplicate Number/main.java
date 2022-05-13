f263fbc4b660c92de27f68866129f009a17e76fa
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-05-13 06:25:38 +0530

    updated

--- Solutions/0287. Find the Duplicate Number/main.java
+++ Solutions/0287. Find the Duplicate Number/main.java
@@ -6,6 +6,19 @@ public class main {
     // TC : O(n)
     // SC: O(1)
     public int findDuplicate(int[] nums) {
-        
+        int k = 0;
+
+        while (k < nums.length) {
+            int correct = nums[k] - 1;
+            if (nums[k] != nums[correct]) {
+                int temp = nums[k];
+                nums[k] = nums[correct];
+                nums[correct] = temp;
+            } else {
+                k++;
+            }
+        }
+
+        return nums[nums.length - 1];
     }
 }
