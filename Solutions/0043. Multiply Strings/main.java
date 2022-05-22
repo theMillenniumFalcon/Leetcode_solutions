0e4aab50151261f34feca8ce8a3b18576a15af40
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-05-23 01:10:33 +0530

    updated

--- Solutions/0043. Multiply Strings/main.java
+++ Solutions/0043. Multiply Strings/main.java
@@ -3,45 +3,43 @@ public class main {
         
     }
 
+    // TC : O(num1 * num2)
+    // SC: O(num1 + num2)
     public String multiply(String num1, String num2) {
         int m = num1.length();
-        int n = num2.length();
+		int n = num2.length();
 
-        if (m == 0 || n == 0 || "0".equals(num1) || "0".equals(num2)) {
-            return "0";
-        }
+		if (m == 0 || n == 0 || "0".equals(num1) || "0".equals(num2)) {
+			return "0";
+		}
+		if ("1".equals(num1)) {
+			return num2;
+		}
+		if ("1".equals(num2)) {
+			return num1;
+		}
 
-        if ("1".equals(num1)) {
-            return num2;
-        }
+		int[] result = new int[m + n];
 
-        if ("1".equals(num2)) {
-            return num1;
-        }
+		for (int i = m - 1; i >= 0; i--) {
+			for (int j = n - 1; j >= 0; j--) {
+				int product = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
 
-        int[] result = new int[m + n];
+				product += result[i + j + 1];
 
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                int product = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+				result[i + j + 1] = product % 10;
+				result[i + j] += product / 10;
+			}
+		}
 
-                // Adding previous values in result array into this product
-                product += result[i + j + 1];
+		StringBuilder sb = new StringBuilder();
+		for (int r : result) {
+			if (sb.length() == 0 && r == 0) {
+				continue;
+			}
+			sb.append(r);
+		}
 
-                // Adding the new product into the result array
-                result[i + j + 1] = product % 10;
-                result[i + j] += product / 10;
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for ( int r : result) {
-            if (sb.length() == 0 && r == 0) {
-                continue;
-            }
-            sb.append(r);
-        }
-
-        return sb.toString();
+		return sb.toString();
     }
 }
