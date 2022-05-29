1a6d51123ded3653c57c92abb69fc51e2a7aabf6
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-05-29 14:40:19 +0530

    updated

--- Solutions/0853. Car Fleet/main.java
+++ Solutions/0853. Car Fleet/main.java
@@ -1,11 +1,37 @@
+import java.util.*;
+
 public class main {
     public static void main(String[] args) {
-        
+
     }
 
-    // TC : O(n)
-    // SC: O(1)
+    // TC : O(n logn)
+    // SC: O(n)
     public int carFleet(int target, int[] position, int[] speed) {
-        
+        int n = position.length;
+        int[] dist = new int[n];
+        double[] timeAr = new double[n];
+
+        HashMap<Integer, Double> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            dist[i] = target - position[i]; // calculate distance left for each car
+            timeAr[i] = (double) dist[i] / (double) speed[i]; // calculate original time needed for each car
+            map.put(dist[i], timeAr[i]); // map distance left with original time needed
+        }
+
+        Arrays.sort(dist); // the cars with larger distance left & smaller time needed will chase up the
+                           // car before
+                           
+        List<Double> rTimeL = new ArrayList<Double>(); // store time used for each fleet in ascending order
+
+        for (int i = 0; i < n; i++) {
+            Double time = map.get(dist[i]);
+            int m = rTimeL.size();
+            if (m == 0 || time > rTimeL.get(m - 1)) {
+                rTimeL.add(time); // new fleet is created
+            }
+        }
+        return rTimeL.size();
     }
 }
