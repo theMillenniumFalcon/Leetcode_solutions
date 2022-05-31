6bf36e22d767c051beb21db8f848f0e57ec8416f
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-05-31 05:41:57 +0530

    updated

---
+++ Solutions/0703. Kth Largest Element in a Stream/main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class KthLargest {
+
+    int k;
+    PriorityQueue<Integer> minHeap;
+
+    // TC : O(n log n)
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        minHeap = new PriorityQueue<>((a , b) -> a - b);
+
+        for (int num : nums) {
+            minHeap.add(num);
+        }
+        while (minHeap.size() > k) {
+            minHeap.poll();
+        }
+    }
+    
+    // TC : O(log n)
+    public int add(int val) {
+        minHeap.add(val);
+
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+
+        return minHeap.peek();
+    }
+}
