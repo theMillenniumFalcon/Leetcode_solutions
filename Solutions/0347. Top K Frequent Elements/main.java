6556d1362be5f27c8c03e78dcb061ea7d16c3ddc
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-06-03 16:35:57 +0530

    updated

--- Solutions/0347. Top K Frequent Elements/main.java
+++ Solutions/0347. Top K Frequent Elements/main.java
@@ -1,26 +1,34 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class main {
     public static void main(String[] args) {
         
     }
 
-    // TC : O(n)
-    // SC: O(n)
+    // TC : O(nlog k)
+    // SC: O(k)
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        List freq = new ArrayList<>(nums.length + 1);
 
-        for (int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
 
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> (a[1] - b[1]));
 
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            freq[map]
+            minHeap.offer(new int[]{entry.getKey(), entry.getValue()});
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
         }
+        int[] res = new int[k];
+
+        while (k > 0) {
+            res[k - 1] = minHeap.poll()[0];
+            k--;
+        }
+
+        return res;
     }
 }
