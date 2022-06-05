8c98ce5d7dffae7839013afa8add300d21b2b104
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-06-05 23:39:14 +0530

    updated

--- Solutions/0039. Combination Sum/main.java
+++ Solutions/0039. Combination Sum/main.java
@@ -1,12 +1,32 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 public class main {
     public static void main(String[] args) {
-        
+
     }
 
+    // TC : O(n * 2^k), k -> average length
+    // SC: variable
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        
+        List<List<Integer>> res = new ArrayList<>();
+        findCombinations(0, candidates, target, res, new ArrayList<>());
+
+        return res;
+    }
+
+    public void findCombinations(int index, int[] arr, int target, List<List<Integer>> res, List<Integer> ds) {
+        if (index == arr.length) {
+            if (target == 0) {
+                res.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+
+        if (arr[index] <= target) {
+            ds.add(arr[index]);
+            findCombinations(index, arr, target - arr[index], res, ds);
+            ds.remove(ds.size() - 1);
+        }
+        findCombinations(index + 1, arr, target, res, ds);
     }
 }
