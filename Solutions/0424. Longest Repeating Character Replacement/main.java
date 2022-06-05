8e2823c62e79855220678bdf10262b1169a8fa89
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-06-05 17:33:19 +0530

    updated

--- Solutions/0424. Longest Repeating Character Replacement/main.java
+++ Solutions/0424. Longest Repeating Character Replacement/main.java
@@ -1,9 +1,24 @@
 public class main {
     public static void main(String[] args) {
-        
+
     }
 
+    // TC : O(n)
+    // SC: O(1)
     public int characterReplacement(String s, int k) {
-        
+        char[] arr = s.toCharArray();
+        int left = 0, right = 0, maxCount = 0, arrLen = arr.length;
+
+        int[] alpha = new int[26];
+
+        while (right < arrLen) {
+            alpha[arr[right] - 'A']++;
+            maxCount = Math.max(maxCount, alpha[arr[right] - 'A']);
+            while (right - left + 1 - maxCount > k)
+                alpha[arr[left++] - 'A']--;
+            right++;
+        }
+
+        return s.length() - left;
     }
 }
