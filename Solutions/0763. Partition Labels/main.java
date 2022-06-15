0743224f143f117d5d4f1af4e799a5a6b9a10cd4
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-06-16 01:46:11 +0530

    updated

--- Solutions/0763. Partition Labels/main.java
+++ Solutions/0763. Partition Labels/main.java
@@ -5,23 +5,28 @@ public class main {
         
     }
 
-    // TC : O(2^n)
-    // SC: O(n)
+    // TC : O(n)
+    // SC: O(1)
     public List<Integer> partitionLabels(String s) {
-        List<Integer> partitionLength = new ArrayList<>();
-        int[] lastIndex = new int[26];
+        if (s == null || s.length() == 0) return null;
+        List<Integer> res = new ArrayList<>();
 
-        for (int i = 0;i < s.length(); i++) {
-            lastIndex[s.charAt(i) - 'a'];
+        int[] last_index = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            last_index[s.charAt(i) - 'a'] = i;
         }
 
-        int i = 0;
-        while (i < s.length()) {
-            int end = lastIndex[s.charAt(i) - 'a'];
-            int j = i + 1;
-            while (j != end) {
-                end = Math.max(end, )
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, last_index[s.charAt(i) - 'a']);
+            if (i == end) {
+                res.add(end - start + 1);
+                start = end + 1;
             }
         }
+
+        return res;
     }
 }
