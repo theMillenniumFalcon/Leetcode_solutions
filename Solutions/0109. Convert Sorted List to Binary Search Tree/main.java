af29cf04e4868a65d9245d70c49e68cfd782ab07
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-06-19 19:44:05 +0530

    updated

---
+++ Solutions/0109. Convert Sorted List to Binary Search Tree/main.java
@@ -0,0 +1,66 @@
+import java.util.Locale.IsoCountryCode;
+
+public class main {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    // TC : O(n)
+    // SC: O(1), O(n) -> auxiliary space
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return new TreeNode(head.val);
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode mid = slow.next;
+        slow.next = null;
+
+        TreeNode root = new TreeNode(mid.val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(mid.next);
+
+        return root;
+    }
+}
