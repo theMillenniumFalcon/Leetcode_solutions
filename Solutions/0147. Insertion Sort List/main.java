d39ffe6bde9800ce95a2ba3714e848ad63629f97
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-06-23 21:40:56 +0530

    updated

--- Solutions/0147. Insertion Sort List/main.java
+++ Solutions/0147. Insertion Sort List/main.java
@@ -20,12 +20,35 @@ public class main {
 
     }
 
-    // TC : O(m*n * log(D)) , D stand min(m, n)
-    // SC: O(m*n)
+    // TC : O(n^2)
+    // SC: O(1)
     public ListNode insertionSortList(ListNode head) {
         if (head == null || head.next == null) return head;
 
-        ListNode dummyHead = new ListNode(-1);
-    }
+        ListNode preInsert;
+        ListNode toInsert;
+        ListNode dummy = new ListNode(Integer.MIN_VALUE);
+        dummy.next = head;
+        ListNode currIt = head;
 
+        while (currIt != null && currIt.next != null) {
+            if (currIt.val <= currIt.next.val) {
+                currIt = currIt.next;
+            } else {
+                // out of sorting order
+                toInsert = currIt.next;
+                //locate preInsert
+                preInsert = dummy;
+                while (preInsert.next.val < toInsert.val) {
+                    preInsert = preInsert.next;
+                }
+                currIt.next = toInsert.next;
+                // Insert toInsert after preInsert
+                toInsert.next = preInsert.next;
+                preInsert.next = toInsert;
+            }
+        }
+
+        return dummy.next;
+    }
 }
