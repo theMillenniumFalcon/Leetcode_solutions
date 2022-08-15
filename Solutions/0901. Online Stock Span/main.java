2104a3157be04db89dd7285f701d0bc596311363
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-08-15 08:20:35 +0530

    updated

---
+++ Solutions/0901. Online Stock Span/main.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+class StockSpanner {
+
+    // TC : O(n)
+    // SC: O(n)
+    class Pair {
+        int price;
+        int index;
+    }
+
+    Stack<Pair> stack;
+    int time = 0;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+        Pair base = new Pair();
+        base.price = 1000000;
+        base.index = -1;
+        stack.push(base);
+    }
+
+    public int next(int price) {
+        Pair pair = new Pair();
+        pair.price = price;
+        pair.index = time;
+        time++;
+
+        while (stack.peek().price <= pair.price) {
+            stack.pop();
+        }
+
+        int ans = pair.index - stack.peek().index;
+
+        stack.push(pair);
+
+        return ans;
+    }
+}
