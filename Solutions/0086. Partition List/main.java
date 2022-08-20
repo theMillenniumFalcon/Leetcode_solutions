43ab69f3674f0f3b7f93cc18d0ca2032632f0e7b
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-08-20 22:30:07 +0530

    updated

---
+++ Solutions/0086. Partition List/main.java
@@ -0,0 +1,48 @@
+public class main {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    // TC : O(n)
+    // SC: O(1)
+    public ListNode partition(ListNode head, int x) {
+        ListNode before_head = new ListNode(0);
+        ListNode before = before_head;
+        ListNode after_head = new ListNode(0);
+        ListNode after = after_head;
+
+        while (head != null) {
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                after.next = head;
+                after = after.next;
+            }
+
+            head = head.next;
+        }
+
+        after.next = null;
+        before.next = after_head.next;
+
+        return before_head.next;
+    }
+}
