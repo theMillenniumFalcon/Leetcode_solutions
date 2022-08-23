28d3b6d3c7b7bbf74287fc5f0109a987d1f748a4
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-08-23 16:48:36 +0530

    updated

--- Solutions/0015. 3Sum/main.java
+++ Solutions/0015. 3Sum/main.java
@@ -5,9 +5,38 @@ public class main {
 
     }
 
-    // TC : O(n^2 + nlog n) = O(n^2)
-    // SC: O(1) or O(n), O(n) would be used in sorting
+    // TC : O(n^2)
+    // SC: O(1)
     public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
 
+        List<List<Integer>> res = new LinkedList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int lowPointer = i + 1, highPointer = nums.length - 1, sum = 0 - nums[i];
+
+                while (lowPointer < highPointer) {
+                    if (nums[lowPointer] + nums[highPointer] == sum) {
+                        res.add(Arrays.asList(nums[i], nums[lowPointer], nums[highPointer]));
+
+                        while (lowPointer < highPointer && nums[lowPointer] == nums[lowPointer + 1]) {
+                            lowPointer++;
+                        }
+                        while (lowPointer < highPointer && nums[highPointer] == nums[highPointer - 1]) {
+                            highPointer--;
+                        }
+                        lowPointer++;
+                        highPointer--;
+                    } else if (nums[lowPointer] + nums[highPointer] < sum) {
+                        lowPointer++;
+                    } else {
+                        highPointer--;
+                    }
+                }
+            }
+        }
+
+        return res;
     }
 }
