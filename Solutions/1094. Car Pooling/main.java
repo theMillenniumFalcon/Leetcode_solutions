16f546ee6f32602a18ddddea8b6cb27f4e8dcd76
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-08-24 12:07:02 +0530

    updated

---
+++ Solutions/1094. Car Pooling/main.java
@@ -0,0 +1,25 @@
+public class main {
+    public static void main(String[] args) {
+
+    }
+
+    // TC : O(n)
+    // SC: O(1)
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] occupancy = new int[1001];
+
+        for (int i = 0; i < trips.length; i++) {
+            occupancy[trips[i][1]] += trips[i][0];
+            occupancy[trips[i][2]] -= trips[i][0];
+        }
+        int currCapacity = 0;
+        for (int currOccupancy : occupancy) {
+            currCapacity += currOccupancy;
+            if (currCapacity > capacity) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
