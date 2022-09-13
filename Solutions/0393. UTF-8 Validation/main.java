e27b56641255309a9130d37e7ac0fd3b0b2a7b35
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-09-13 14:23:54 +0530

    updated

---
+++ Solutions/0393. UTF-8 Validation/main.java
@@ -0,0 +1,38 @@
+public class main {
+    public static void main(String[] args) {
+
+    }
+
+    // TC: O(n), n -> length of data
+    // SC: O(1)
+    public boolean validUtf8(int[] data) {
+        int remBytes = 0;
+        for (int var : data) {
+            if (remBytes == 0) {
+                if ((var >> 7) == 0b0) { // 1byte
+                    remBytes = 0;
+                } else if ((var >> 5) == 0b110) { // 2 byte
+                    remBytes = 1;
+                } else if ((var >> 4) == 0b1110) { // 3 byte
+                    remBytes = 2;
+                } else if ((var >> 3) == 0b11110) { // 4byte
+                    remBytes = 3;
+                } else {
+                    return false;
+                }
+            } else {
+                if ((var >> 6) == 0b10) {
+                    remBytes--;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (remBytes == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
