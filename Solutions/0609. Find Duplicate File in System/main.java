3a3dccb69389a3de3da047395d02b4a347ee6110
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-09-19 09:56:13 +0530

    updated

---
+++ Solutions/0609. Find Duplicate File in System/main.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+
+    // TC: O(n), n -> leength of paths
+    // SC: O(k), k -> diffferent types of content
+    public List<List<String>> findDuplicate(String[] paths) {
+        // <Content, List<FilePaths>>
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String path : paths) {
+            String[] dir = path.split(" ");
+            String rootPath = dir[0];
+
+            for (int i = 1; i < dir.length; i++) {
+                // 1.txt(abcd)
+                String content = dir[i].substring(dir[i].indexOf("("), dir[i].indexOf(")"));
+
+                if (!map.containsKey(content)) {
+                    map.put(content, new ArrayList<>());
+                }
+
+                map.get(content).add(rootPath + "/" + dir[i].substring(0, dir[i].indexOf("(")));
+
+            }
+        }
+
+        List<List<String>> res = new ArrayList<>();
+
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                res.add(entry.getValue());
+            }
+        }
+
+        return res;
+    }
+}
