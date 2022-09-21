dd7816ba60d408ccbe896e3e267657460ad07c21
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-09-21 12:19:11 +0530

    updated

---
+++ Solutions/0718. Maximum Length of Repeated Subarray/main.java
@@ -0,0 +1,26 @@
+public class main {
+    public static void main(String[] args) {
+
+    }
+
+    // TC: O(n * m), n -> length of nums1, m -> length of nums2
+    // SC: O(n * m), n -> length of nums1, m -> length of nums2
+    public int findLength(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int res = 0;
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    res = Math.max(res, dp[i][j]);
+                }
+            }
+        }
+
+        return res;
+    }
+}
