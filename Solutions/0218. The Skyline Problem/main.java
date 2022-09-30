590e40160c12b5f1f1f89e27f36e38e46e60f7f3
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-09-30 22:28:41 +0530

    updated

---
+++ Solutions/0218. The Skyline Problem/main.java
@@ -0,0 +1,48 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+
+    // TC: O(n * log(n))
+    // SC: O(1)
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<int[]> heights = new ArrayList<>();
+
+        transformBuilding(buildings, heights); // O(n)
+        Collections.sort(heights, (a, b) -> (a[0] == b[0]) ? a[1] - b[1] : a[0] - b[0]); // O(nlogn)
+
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a, b) -> (b - a));
+        pq.offer(0);
+        int preMax = 0;
+        for (int[] height : heights) {
+            if (height[1] < 0) {
+                pq.offer(-height[1]);
+            } else {
+                pq.remove(height[1]); // O(logn)
+            }
+            int currMax = pq.peek();
+            if (currMax != preMax) {
+                List<Integer> subRes = new ArrayList<>();
+                subRes.add(height[0]);
+                subRes.add(currMax);
+                res.add(subRes);
+                preMax = currMax;
+            }
+        }
+        return res;
+    }
+
+    public void transformBuilding(int[][] buildings, List<int[]> heights) {
+
+        for (int[] building : buildings) {
+            heights.add(new int[] { building[0], -building[2] });
+            heights.add(new int[] { building[1], building[2] });
+        }
+    }
+}
