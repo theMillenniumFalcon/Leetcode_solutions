c78c445452c54fdf9c57e598f4c2df9fb5f9d9a7
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-10-01 10:05:11 +0530

    updated

---
+++ Solutions/0091. Decode Ways/main.java
@@ -0,0 +1,28 @@
+public class main {
+    public static void main(String[] args) {
+
+    }
+
+    // TC: O(n), n -> length of string s
+    // SC: O(n), n -> length of string s
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i <= s.length(); i++) {
+            int oneDigit = Integer.valueOf(s.substring(i - 1, i));
+            int twoDigits = Integer.valueOf(s.substring(i - 2, i));
+
+            if (oneDigit >= 1) {
+                dp[i] += dp[i - 1];
+            }
+
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
