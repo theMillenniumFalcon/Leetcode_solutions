1eba042f9b7de71e31efb75c41de2c66590a9180
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-10-13 09:23:52 +0530

    updated

--- Solutions/0237. Delete Node in a Linked List/main.java
+++ Solutions/0237. Delete Node in a Linked List/main.java
@@ -8,6 +8,8 @@ class Solution {
         }
     }
 
+    // TC: O(1)
+    // SC: O(1)
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
         node.next = node.next.next;
