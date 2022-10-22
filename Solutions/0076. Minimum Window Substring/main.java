2eb27e3b4d5afc574c07591385630421dd55e46a
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-10-22 11:48:12 +0530

    updated

--- Solutions/0076. Minimum Window Substring/main.java
+++ Solutions/0076. Minimum Window Substring/main.java
@@ -1,70 +1,37 @@
-import java.util.HashMap;
-
 public class main {
     public static void main(String[] args) {
 
     }
 
-    // TC : O(n)
-    // SC: O(n)
+    // TC : O(s + t), s -> length of string s, t -> length of string t
+    // SC: O(1)
     public String minWindow(String s, String t) {
-        String ans = "";
-
-        HashMap<Character, Integer> map2 = new HashMap<>();
-
-        for (int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
-            map2.put(ch, map2.getOrDefault(ch, 0) + 1);
+        int[] freq = new int[128];
+        for (char c : t.toCharArray()) {
+            freq[c]++;
         }
-
-        int matchCount = 0;
-        int desiredMatchCount = t.length();
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        int i = -1;
-        int j = -1;
-
-        while (true) {
-            boolean flag1 = false;
-            boolean flag2 = false;
-
-            // acquire
-            while (i < s.length() - 1 && matchCount < desiredMatchCount) {
-                i++;
-                char ch = s.charAt(i);
-                map1.put(ch, map1.getOrDefault(ch, 0) + 1);
-
-                if (map1.getOrDefault(ch, 0) <= map2.getOrDefault(ch, 0)) {
-                    matchCount++;
-                }
-                flag1 = true;
+        int start = 0, end = 0, minStart = 0, minLen = Integer.MAX_VALUE, counter = t.length();
+        while (end < s.length()) {
+            char endS = s.charAt(end);
+            if (freq[endS] > 0) {
+                counter--;
             }
-
-            // collect answer and release
-            while (j < i && matchCount == desiredMatchCount) {
-                String potentialAns = s.substring(j + 1, i + 1);
-                if (ans.length() == 0 || potentialAns.length() < ans.length()) {
-                    ans = potentialAns;
-                }
-                j++;
-                char ch = s.charAt(j);
-
-                if (map1.get(ch) == 1) {
-                    map1.remove(ch);
-                } else {
-                    map1.put(ch, map1.get(ch) - 1);
+            freq[endS]--;
+            end++;
+            while (counter == 0) {
+                if (minLen > end - start) {
+                    minLen = end - start;
+                    minStart = start;
                 }
 
-                if (map1.getOrDefault(ch, 0) < map2.getOrDefault(ch, 0)) {
-                    matchCount--;
-                }
-                flag2 = true;
-            }
-
-            if (flag1 == false && flag2 == false) {
-                break;
+                char startS = s.charAt(start);
+                freq[startS]++;
+                if (freq[startS] > 0)
+                    counter++;
+                start++;
             }
         }
 
-        return ans;
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
     }
 }
