2d8c1cb8f3102d575d247ce70ba9a0b6f27edea6
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-10-23 09:52:56 +0530

    updated

--- Solutions/0645. Set Mismatch/main.java
+++ Solutions/0645. Set Mismatch/main.java
@@ -1,34 +1,32 @@
+import java.util.HashSet;
+
 public class main {
     public static void main(String[] args) {
-        // missing = index + 1
-        // duplicate = no. at the missing indexs
-        
+
     }
 
-    static int[] findErrorNums(int[] nums) {
-        int i = 0;
-        while (i < nums.length) {
-            int correct = nums[i] - 1;
-            if (nums[i] != nums[correct]) {
-                swap(nums, i, correct);
-            } else {
-                i++;
+    // TC: O(n), n -> length of nums
+    // SC: O(n), n -> length of nums
+    public int[] findErrorNums(int[] nums) {
+        int duplicate = 0;
+        int miss = 0;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (set.contains(nums[i])) {
+                duplicate = nums[i];
+            }
+            set.add(nums[i]);
+        }
+
+        for (int i = 1; i <= nums.length; ++i) {
+            if (!set.contains(i)) {
+                miss = i;
+                break;
             }
         }
 
-        // search for the first missing number
-        for (int index = 0; index < nums.length; index++) {
-            if (nums[index] != index + 1) {
-                return new int[]{nums[index], index + 1};
-            }
-        }
-        return new int[]{-1, -1};
+        return new int[] { duplicate, miss };
     }
-
-    static void swap(int[] nums, int first, int second) {
-        int temp = nums[first];
-        nums[first] = nums[second];
-        nums[second] = temp;
-    }
-    
 }
