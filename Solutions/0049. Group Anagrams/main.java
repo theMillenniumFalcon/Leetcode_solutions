1eb28e743cb4d2514d8eb53bb3b43be49882d540
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-10-28 10:57:08 +0530

    updated

--- Solutions/0049. Group Anagrams/main.java
+++ Solutions/0049. Group Anagrams/main.java
@@ -1,29 +1,36 @@
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class main {
     public static void main(String[] args) {
 
     }
 
-    // TC : O(m * n) n -> Average length of strings; m -> Total number of strings
-    // SC: O(n)
+    // TC : O(n * m * log(m)), n -> the max number of words we can receive in
+    // "strs"; m -> largest size a word in "strs" can be
+    // SC: O(n), n -> the max number of words we can receive in
+    // "strs"
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> res = new ArrayList<>();
         HashMap<String, List<String>> map = new HashMap<>();
-        if (strs.length == 0) return res;
-        for (String s : strs) {
-            char[] hash = new char[26];
-            for (char ch : s.toCharArray()) {
-                hash[ch - 'a']++;
-            }
-            String str = new String(hash);
-            if (map.get(str) == null) {
-                map.put(str, new ArrayList<>());
-            }
-            map.get(str).add(s);
+        if (strs.length == 0) {
+            return res;
         }
+        for (String current : strs) {
+            char[] characters = current.toCharArray();
+            Arrays.sort(characters);
+            String sorted = new String(characters);
 
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList<>());
+            }
+
+            map.get(sorted).add(current);
+        }
         res.addAll(map.values());
+
         return res;
     }
 }
