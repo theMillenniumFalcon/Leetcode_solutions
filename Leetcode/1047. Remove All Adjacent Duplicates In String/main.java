5bd2bcd4484debf511c28e5878e403fe1758437f
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-11-10 19:43:37 +0530

    updated

--- Leetcode/1047. Remove All Adjacent Duplicates In String/main.java
+++ Leetcode/1047. Remove All Adjacent Duplicates In String/main.java
@@ -1,23 +1,32 @@
+import java.util.Stack;
+
 public class main {
     public static void main(String[] args) {
-        
+
     }
 
+    // TC: O(n), n -> length of string s
+    // SC: O(n), n -> length of string s
     public String removeDuplicates(String s) {
-        char[] stack = new char[s.length()];
-        int i = 0;
+        Stack<Character> stack = new Stack<>();
 
-        for (int j = 0; j < s.length(); j++) {
-            char current_char = s.charAt(j);
-
-            if (i > 0 && stack[i-1] == current_char) {
-                i--;
+        for (int i = 0; i < s.length(); i++) {
+            if (!stack.isEmpty()) {
+                if (stack.peek() == s.charAt(i)) {
+                    stack.pop();
+                } else {
+                    stack.push(s.charAt(i));
+                }
             } else {
-                stack[i] = current_char;
-                i++;
+                stack.push(s.charAt(i));
             }
         }
 
-        return new String(stack, 0, i);
+        StringBuilder sb = new StringBuilder("");
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.reverse().toString();
     }
 }
